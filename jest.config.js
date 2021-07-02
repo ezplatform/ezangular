fe34528862b7871c73ad1a63244de474f4448c02
Kay - Khanh Bùi <khanhbui.lab@gmail.com>
2021-07-02 22:47:11 +0700

    feat(repo): initialize apps

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/sysadmin',
+    '<rootDir>/apps/investor',
+    '<rootDir>/apps/sysadmin-api',
+    '<rootDir>/apps/investor-api',
+  ],
+};
