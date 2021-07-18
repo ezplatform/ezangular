3a0460e57e1d94a7b636073453a0b126a0fe23c8
Kay - Khanh Bùi <khanhbui.lab@gmail.com>
2021-07-18 20:25:20 +0700

    chore(styling): add ng-zorro-ant layout

--- apps/styling/src/app/app.module.ts
+++ apps/styling/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IconsProviderModule } from './icons-provider.module';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
-  providers: [],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
